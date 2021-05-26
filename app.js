b2b7bbb547d3190680304fa64eece94af3446139
Muhammad Adeel <adeelshaikh654@gmail.com>
2021-05-26 17:52:34 +0500

    First commit of chap

---
+++ app.js
@@ -0,0 +1,66 @@
+// Question:1
+// var posNum = +prompt("Enter any positive number");
+
+// document.write("Number: " + Number(posNum) + "<br>");
+// document.write("Round: " + Math.round(posNum) + "<br>");
+// document.write("Floor: " + Math.floor(posNum) + "<br>");
+// document.write("Ceil: " + Math.ceil(posNum));
+
+// Question:2
+// var negNum = +prompt("Enter any negative float number");
+
+// document.write("Number: " + Number(negNum) + "<br>");
+// document.write("Round: " + Math.round(negNum) + "<br>");
+// document.write("Ceil: " + Math.ceil(negNum) + "<br>");
+// document.write("Floor: " + Math.floor(negNum) + "<br>");
+
+// Question:3
+// var negNum = +prompt("Enter any number in +/-");
+// console.log(Math.abs(negNum));
+
+// Question:4
+// var dice = Math.random() * 6;
+// document.write("Random dice value: " + Math.round(dice))
+
+
+// Question:5
+// var coin = Math.random() * 2;
+// var coinRnd = Math.ceil(coin);
+
+// if(coinRnd == 1){
+//     document.write(coinRnd);
+//     document.write("<br>"+ "Random Coin Value: Heads");
+// }
+// else{
+//     document.write(coinRnd);
+//     document.write("<br>"+ "Random Coin Value: Tails");
+// }
+
+// Question:6
+// var ranNum = Math.random() * 100;
+// document.write("Random Number between 1 and 100: " + Math.round(ranNum))
+
+// Question:7
+// var userWeight = prompt("Enter weight in Kg");
+// var weightConvert = parseInt(userWeight);
+// document.write("The weight of the user is " + weightConvert + " kilograms")
+
+
+// Question:8
+
+// var secNum = Math.random() * 10;
+// var secnumRound = Math.ceil(secNum)
+// console.log(secnumRound)
+// var userNum = +prompt("Enter any num from 1 to 10");
+
+// if(userNum > 0 && userNum <= 10){
+//     if(userNum === secnumRound){
+//         alert("Congratulations you have guess the correct number");
+//     }
+//     else{
+//            alert("Sorry you have guess the correct number");
+//     } 
+// }
+// else{
+//     alert("Wrong input")
+// }
